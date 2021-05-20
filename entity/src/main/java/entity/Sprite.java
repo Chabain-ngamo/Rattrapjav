7ccefdad6b8942b4955549db2ab5274197f325ce
media <media@DESKTOP-KRMBOSN>
2021-05-20 14:49:48 +0100

    Phase 3

--- entity/src/main/java/entity/Sprite.java
+++ entity/src/main/java/entity/Sprite.java
@@ -1,5 +1,133 @@
+/**
+ * 
+ */
 package entity;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javax.swing.ImageIcon;
+
+/**
+ * <h1> The Sprite Class</h1>
+ * @author media
+ *
+ */
 public class Sprite {
+	/**
+	 * The image
+	 */
+	private ImageIcon image;
+	
+
+	/**
+	 * The name of the image
+	 */
+	private String imageName;
+	/**
+	 * Path to the sprite
+	 */
+	private String spritePath;
+	/**
+	 * Reference of the sprite
+	 */
+	private char sprite_ref;
+
+
+
+
+	
+
+	/**
+	 * Instantiates a new Sprite.
+	 * @param sprite_ref The ref of the sprite
+	 * @param spritePath The Path of the sprite
+	 * @param imageName The Image Name
+	 */
+	public Sprite(char sprite_ref, String spritePath, String imageName) {
+		this.setSprite_ref(sprite_ref);
+		this.setImageName(imageName);
+		this.setSpritePath(spritePath);
+	}
+
+	/**
+	 * Gets the image.
+	 * @return The {@link #image}
+	 */
+	public ImageIcon getImage() {
+		return this.image;
+	}
+
+	/**
+	 * Sets the image.
+	 * @param myPicture The image
+	 */
+	public void setImage(BufferedImage myPicture) {
+		image = new ImageIcon(myPicture);
+	}
+
+	/**
+	 * Loads the image from the /Sprites folder.
+	 */
+	public void loadImage() {
+		String absolutePath = new File("").getAbsolutePath();
+		try {
+			this.setImage(ImageIO.read(new File(absolutePath+this.getSpritePath()+this.getImageName())));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Gets the Image name
+	 * @return The {@link #imageName}
+	 */
+	public String getImageName() {
+		return this.imageName;
+	}
+
+	/**
+	 * Sets the image name
+	 * @param imageName The Image Name.
+	 */
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	/**
+	 * Gets the path to the sprite
+	 * @return The path to the sprite.
+	 */
+	public String getSpritePath() {
+		return this.spritePath;
+	}
+
+	/**
+	 * Sets the path to the sprite
+	 * @param spritePath The path to the sprite
+	 */
+	public void setSpritePath(String spritePath) {
+		this.spritePath = spritePath;
+	}
+
+	/**
+	 * Gets the sprite reference.
+	 * @return The {@link #sprite_ref}
+	 */
+	public char getSprite_ref() {
+		return this.sprite_ref;
+	}
+
+	/**
+	 * Sets the Sprite reference.
+	 * @param sprite_ref The sprite reference.
+	 */
+	public void setSprite_ref(char sprite_ref) {
+		this.sprite_ref = sprite_ref;
+	}
+	
 
 }
---
+++ entity/src/test1/java/entity/SpriteTest.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * The test class for the Sprite class.
+ * @author media
+ *
+ */
+public class SpriteTest {
+	Sprite sprite;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/** 
+	 * This will set up once all of the test methods in the class are done.
+	 * It is used to release the allocathon that the BeforeClass method allocates. It runs even if the BeforeClass method throws an exception.
+	 * 
+	 * @throws Exception Every Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 *  Sets up the method before each Test method.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		sprite = new Sprite('O', "/sprites/settings/", "diamonds.png");
+	}
+
+	/**
+	 * Sets up after each Test method.
+	 * It is used to release the allocathon that the Before method allocates. It runs even if the Before method throws an exception.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+
+	
+
+	/**
+	 * Test method for {@link entity.element.Sprite#loadImage()}.
+	 */
+	@Test
+	public void testLoadImage() {
+		this.sprite.loadImage();
+		Assert.assertNotNull(this.sprite.getImage());
+	}
+
+	
+
+	/**
+	 * Test method for {@link entity.element.Sprite#setImageName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetImageName() {
+		String expected ="diamonds.png";
+		this.sprite.setImageName(expected);
+		Assert.assertEquals(expected, this.sprite.getImageName());
+	}
+
+	/**
+	 * Test method for {@link entity.element.Sprite#setSpritePath()}.
+	 */
+	
+	@Test
+	public void testSetSpritePath() {
+		String expected = "/sprites/settings/";
+		this.sprite.setSpritePath(expected);
+		Assert.assertEquals(expected, this.sprite.getSpritePath());
+		
+	}
+
+	
+	/**
+	 * Test method for {@link entity.element.Sprite#setSprite_ref(char)}.
+	 */
+	@Test
+	public void testSetSprite_ref() {
+		char expected = 'O';
+		this.sprite.setSprite_ref(expected);
+		Assert.assertEquals(expected, this.sprite.getSprite_ref());
+	}
+
+}
+
