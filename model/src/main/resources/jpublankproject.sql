ba18a6642b6f37924b9e4f42bf057e926ae683bb
Administrateur <Administrateur@DESKTOP-FGS1U75>
2021-05-24 16:48:38 +0100

    phase 10

--- model/src/main/resources/jpublankproject.sql
+++ model/src/main/resources/jpublankproject.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Jeu 02 Juin 2016 à 23:02
--- Version du serveur :  5.7.11
--- Version de PHP :  5.6.19
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 04, 2020 at 02:04 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,68 +19,46 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `jpublankproject`
+-- Database: `jpublankproject`
 --
-CREATE DATABASE `jpublankproject` ;
-
-USE `jpublankproject` ;
 
 DELIMITER $$
 --
--- Procédures
+-- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `helloworldById` (IN `p_id` INT)  READS SQL DATA
+DROP PROCEDURE IF EXISTS `mapById`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `mapById` (IN `p_id` INT)  READS SQL DATA
     SQL SECURITY INVOKER
-SELECT * FROM helloworld WHERE id = p_id$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `HelloworldByCode` (IN `p_code` VARCHAR(2))  READS SQL DATA
-    SQL SECURITY INVOKER
-SELECT * FROM jpublankproject.helloworld where `code`=p_code$$
+SELECT * FROM Game WHERE id = p_id$$
 
 DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `helloworld`
+-- Table structure for table `levels`
 --
 
-CREATE TABLE `helloworld` (
-  `id` int(11) NOT NULL,
-  `code` varchar(2) NOT NULL,
-  `message` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `Game`;
+CREATE TABLE IF NOT EXISTS `Game` (
+  `id` int(2) NOT NULL AUTO_INCREMENT,
+  `levelContent` text NOT NULL,
+  `numberDiamondsNeeded` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `helloworld`
+-- Dumping data for table `levels`
 --
 
-INSERT INTO `helloworld` (`id`, `code`, `message`) VALUES
-(1, 'GB', 'Hello world'),
-(2, 'FR', 'Bonjour le monde'),
-(3, 'DE', 'Hallo Welt'),
-(4, 'ID', 'Salamat pagi dunia');
+INSERT INTO `Game` (`id`, `levelContent`, `numberDiamondsNeeded`) VALUES
+(1, 'wwwwwwwwwwwwwwwwwwwwwwwww\r\nwdsddsddwdsdddDdwdddpdsdw\r\nwdddddddwddddddpwppdpdddw\r\nwadddddddddddddsdssdddddw\r\nwDddddddwdsddddswdsdpddpw\r\nwdddddddwdsddddswdsdppddw\r\nwwwwwwwwwwwwwwwwwwwwpwwww\r\nwddddssdwddpddddwdddpddsw\r\nwdddddddwddpddddwdddpdddw\r\nwWpppppppppsppppppppppppw\r\nwsddsdddwddddsddwddssdddw\r\nwsddddsdwddsddsdwdddsdssw\r\nwdsddDddwdddsDddwdddddddw\r\nwwwwwwwwwwwwwwwwwwwwwwwww', 4),
+(2, 'wwwwwwwwwwwwwwwwwwwwwwwww\r\nwddddssdwddpddddwdddpddsw\r\nwdddddddwddpddddwdddpdddw\r\nwapppppppppsppppppppppppw\r\nwsddsdddwddddsddwddssdddw\r\nwsddddsdwddsddsdwdddsdssw\r\nwdsddDddwdddsDddwdddddddw\r\nwwwwwwwwwwwwwwwwwwppwwwww\r\nwsWpepddwddddsdswdddpdddw\r\nwdddddpdwpddddddwddDpdddw\r\nwppppppppsppppppppppppDpw\r\nwdDdddspwddpddddwddDpsddw\r\nwdsdWddswddsddsdwddddddDw\r\nwwwwwwwwwwwwwwwwwwwwwwwww', 7),
+(3, 'wwwwwwwwwwwwwwwwwwwwwwwww\r\nwdddddsddddddwwddddsDdddw\r\nwddwdwdwddwddwdwddwddwwdw\r\nwawdswwwdddddswddddddwwww\r\nwdwdDwdwdwwwdwdwdwwwdwwdw\r\nwddwdwdwDwdwDwwddsdwdwwdw\r\nwdddddddddddddddddddDdddw\r\nwwwwppwwwwwwwwwwwwwppwwww\r\nwdddddddwdddssdwddddpppdw\r\nwddddpppwdDsdddwdddspepsw\r\nwddsdpeppddddddwddDdpppdw\r\nwDsddpppwddsddDwddddddddw\r\nwdddddddwddddWdwddddddddw\r\nwwwwwwwwwwwwwwwwwwwwwwwww', 9),
+(4, 'wwwwwwwwwwwwwwwwwwwwwwwww\r\nwdddddsddddwdsddddddddddw\r\nwdddddddwdddddddddwddsddw\r\nwdswdwdddddwdwDwdddddsDdw\r\nwaswwwdwwwdwdwdwdwwwdsddw\r\nwddwDwdwdwdwddwddwdwdddsw\r\nwddppdddddpepdsdsdddddddw\r\nwwwppwwwwwwwwDwpwwddwwwww\r\nwdddsdwdDdddwpwpwdwdddddw\r\nwdspppwddsdswwwwwddwdsddw\r\nwdspepwdddwddddddDddsdddw\r\nwdDpppwdddwdpepddddsdpddw\r\nwsdddDwdWdwdddddddsddwddw\r\nwwwwwwwwwwwwwwwwwwwwwwwww', 8),
+(5, 'wwwwwwwwwwwwwwwwwwwwwwwww\r\nwdddddwddwddddddsdddddddw\r\nwddwdwwdddddwwdwdwDwdwwww\r\nwadwdwwdwdwdwdwwdwwwdwwwd\r\nwdddwdwwwdwdwwdwdwdwdwwpw\r\nwddddsddddddddddddddddddw\r\nwdwwddddwwwwwwwwwwwwwssdw\r\nwddDwddwddddddddddddwdddw\r\nwwwwddwddwwwwwwpeppwddwdw\r\nwdsddwDdwWddddddddwddwddw\r\nwdddwdddwwwwwwwwwwddwwdsw\r\nwDdwddswddssssdddddwDwpdw\r\nwdwdddddddddddddddwdddpdw\r\nwwwwwwwwwwwwwwwwwwwwwwwww', 5);
+COMMIT;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `helloworld`
---
-ALTER TABLE `helloworld`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `code_UNIQUE` (`code`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `helloworld`
---
-ALTER TABLE `helloworld`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
